172a7cb0607f6480f1116addb4be325101a5815a
Tom Roman <tom@tomroman.co.uk>
2021-06-04 18:18:43 +0100

    Fix args indexing

--- src/main/java/network/palace/show/actions/DiscordAction.java
+++ src/main/java/network/palace/show/actions/DiscordAction.java
@@ -45,16 +45,16 @@ public class DiscordAction extends ShowAction {
 
     @Override
     public ShowAction load(String line, String... args) throws ShowParseException {
-        if (args[0] == null) throw new ShowParseException("Invalid channelId " + line);
-        if (args[1] == null) throw new ShowParseException("Invalid title " + line);
-        if (args[2] == null) throw new ShowParseException("Invalid desc " + line);
-        if (args[3] == null) throw new ShowParseException("Invalid startTime " + line);
-        if (args[4] == null) throw new ShowParseException("Invalid whereToWatch " + line);
-        this.channelId = args[0];
-        this.title = args[1];
-        this.desc = args[2];
-        this.startTime = args[3];
-        this.whereToWatch = args[4];
+        if (args[2] == null) throw new ShowParseException("Invalid channelId " + line);
+        if (args[3] == null) throw new ShowParseException("Invalid title " + line);
+        if (args[4] == null) throw new ShowParseException("Invalid desc " + line);
+        if (args[5] == null) throw new ShowParseException("Invalid startTime " + line);
+        if (args[6] == null) throw new ShowParseException("Invalid whereToWatch " + line);
+        this.channelId = args[2];
+        this.title = args[3];
+        this.desc = args[4];
+        this.startTime = args[5];
+        this.whereToWatch = args[6];
         return this;
     }
 
